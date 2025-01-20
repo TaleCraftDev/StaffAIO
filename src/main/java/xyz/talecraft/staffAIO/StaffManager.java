28824b8daab683ce1198eedbbae7316986124367
Joseph Peterson <supercrazysquid@gmail.com>
2025-01-19 23:27:01 -0500

    Added report toggler and prefix from config

--- src/main/java/xyz/talecraft/staffAIO/StaffManager.java
+++ src/main/java/xyz/talecraft/staffAIO/StaffManager.java
@@ -4,16 +4,24 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class StaffManager {
+	Plugin plugin;
+
 	// List of staff members
 	private ArrayList<Player> staffMembers = new ArrayList<>();
 
 	// Reports Enabled
 	private ArrayList<Player> miningReportsEnabled = new ArrayList<>();
+
+	public void init() {
+		this.plugin = JavaPlugin.getPlugin(StaffAIO.class);
+	}
 
 	// Adding and Removing Members
 
@@ -36,6 +44,28 @@ public class StaffManager {
 		miningReportsEnabled.remove(player);
 	}
 
+	// Report Togglers
+
+	/**
+	 * Toggles if the player has mining alerts enabled.
+	 * If the player is not a valid staff member, they will be added.
+	 * @param player Player to check.
+	 * @return If the player has the mining alerts toggled.
+	 */
+	public boolean toggleMiningAlert(Player player) {
+		if(!staffMembers.contains(player)) {
+			addStaffMember(player, true);
+		}
+
+		if(miningReportsEnabled.contains(player)) {
+			miningReportsEnabled.remove(player);
+		} else {
+			miningReportsEnabled.add(player);
+		}
+
+		return miningReportsEnabled.contains(player);
+	}
+
 	// Report Senders
 	/**
 	 * Sends a mining report to subscribed staff members.
@@ -48,7 +78,8 @@ public class StaffManager {
 		boolean realtimeAlerts = xrayConfig.getConfig().getBoolean("alerts.enabled");
 		if(!realtimeAlerts) return;
 
-		String alertMessage = ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(xrayConfig.getConfig().getString("alerts.message")));
+		String alertPrefix = plugin.getConfig().getString("alert_prefix");
+		String alertMessage = ChatColor.translateAlternateColorCodes('&', alertPrefix + Objects.requireNonNull(xrayConfig.getConfig().getString("alerts.message")));
 
 		// Replacing placeholders
 		Location blockLocation = block.getLocation();
